9f1687dcf706563f897852273ba545a8c1672be8
Allison <allison.nancy.brown34@gmail.com>
2024-05-07 14:48:36 -0400

    component layout

---
+++ src/Components/Player.jsx
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+
+
+const PlayBox = styled.div`
+    border: 2px solid red;
+    margin: 0 0 5% 0;
+`
+
+
+export default function Player() {
+
+
+    return (
+        <PlayBox>
+            <h1>Player</h1>
+            <h1>Player</h1>
+            <h1>Player</h1>
+        </PlayBox>
+    )
+}
