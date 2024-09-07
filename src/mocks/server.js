e8fda8cfbd3e679f72d99906847803dad52c23d5
Allison Brown <allison.nancy.brown34@gmail.com>
2024-09-07 16:02:38 -0400

    removed mocks - not needed

--- src/mocks/server.js
+++
@@ -1,6 +0,0 @@
-//not using currently, passed in mock context value directly into components
-
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-export const server = setupServer(...handlers);
